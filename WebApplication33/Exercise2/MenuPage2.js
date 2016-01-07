342711dda8416d819c3bd28a159ecf7325a3e101
SirLopez <daniel.lopez@live.se>
2016-01-07 13:40:09 +0100

    Started over, missunderstod the assignement before adding from start, burgers with literal notation.

--- WebApplication33/Exercise2/MenuPage2.js
+++ WebApplication33/Exercise2/MenuPage2.js
@@ -1,12 +1,52 @@
-﻿var request;
-
-if (window.XMLHttpRequest) {
-    request = new XMLHttpRequest();
-} else {
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-request.open("GET", "Resources/foodArticles.json");
+﻿var sundayBurger = {
+    name: "L-A Burger",
+    size: "mega",
+    weekday: "sunday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
+var mondayBurger = {
+    name: "Daniél Burger",
+    size: "jumbo",
+    weekday: "monday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
+var tuesdayBurger = {
+    name: "Le Roje Burger",
+    size: "big",
+    weekday: "tuesday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
+var wednesdayBurger = {
+    name: "EC Burger",
+    size: "small",
+    weekday: "wednesday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
+var thursdayBurger = {
+    name: "JS Burger",
+    size: "normal",
+    weekday: "thursday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
+var fridayBurger = {
+    name: "Crazy Burger",
+    size: "mega",
+    weekday: "friday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
+var saturdayBurger = {
+    name: "Holiday Burger",
+    size: "jumbo",
+    weekday: "saturday",
+    price: 19.90,
+    img: "Images/Hamburger.png"
+};
 
 
 
@@ -15,6 +55,9 @@ request.open("GET", "Resources/foodArticles.json");
    use attributes name, size, dayOfTheWeek and price(page. 104 in book)
    Please use the same html syntax of an article as in exercise1.
 */
+
+
+
 
 
 /*
