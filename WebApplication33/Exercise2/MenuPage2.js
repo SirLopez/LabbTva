53f98bd3720fe0a94945e8fa7d13bf7066fb71a3
SirLopez <daniel.lopez@live.se>
2016-01-08 12:58:28 +0100

    redid the whole thing, it works now

--- WebApplication33/Exercise2/MenuPage2.js
+++ WebApplication33/Exercise2/MenuPage2.js
@@ -3,76 +3,75 @@
     size: "mega",
     weekday: "sunday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 var mondayBurger = {
     name: "Daniél Burger",
     size: "jumbo",
     weekday: "monday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 var tuesdayBurger = {
     name: "Le Roje Burger",
     size: "big",
     weekday: "tuesday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 var wednesdayBurger = {
     name: "EC Burger",
     size: "small",
     weekday: "wednesday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 var thursdayBurger = {
     name: "JS Burger",
     size: "normal",
     weekday: "thursday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 var fridayBurger = {
     name: "Crazy Burger",
     size: "mega",
     weekday: "friday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 var saturdayBurger = {
     name: "Holiday Burger",
     size: "jumbo",
     weekday: "saturday",
     price: 19.90,
-    img: "Images/Hamburger.png"
+    img: "\Images/Hamburger.png"
 };
 
 var burgers = [sundayBurger, mondayBurger, tuesdayBurger, wednesdayBurger, thursdayBurger, fridayBurger, saturdayBurger];
 
 displayMenu(burgers, "burgersPlace");
 
-function displayMenu(burgers, menu) {
+function displayMenu(products, menu) {
+    var d = document; //just for a nicer codeview, really don´t need it
+    var foodmenu = d.getElementById(menu);
+    
+    for (var i = 0; i < products.length; i++) {
 
-    var foodmenu = document.getElementById(menu);
+        var img = d.createElement("img");
+        img.src = products[i].img;
 
-    for (var i = 0; i < burgers.length; i++) {
+        var article = d.createElement("article")
+            .appendChild(d.createElement("fieldset"))
+            .appendChild(d.createElement("legend"))
+            .appendChild(d.createTextNode(products[i].weekday)).parentNode.parentNode
+            .appendChild(d.createElement("h3"))
+            .appendChild(d.createTextNode(products[i].name)).parentNode.parentNode;
 
-        var legend = document.createElement("legend").appendChild(document.createTextNode(burgers[i].weekday.charAt(0).toUpperCase()));
-        var h3 = document.createElement("h3").appendChild(document.createTextNode(burgers[i].name));
-        var img = document.createElement("img").setAttribute("src", burgers[i].img);
-        var span = document.createElement("span").appendChild(img);
-        var div = document.createElement("div").appendChild(document.createTextNode = burgers[i].price.toFixed(2));
-        var divSize = document.createElement("div").appendChild(document.createTextNode = burgers[i].size);
+        article.appendChild(d.createElement("span")).appendChild(img);
+        article.appendChild(d.createElement("div")).appendChild(d.createTextNode("Price: " + products[i].price.toFixed(2)));
+        article.appendChild(d.createElement("div")).appendChild(d.createTextNode("Size: " + products[i].size));
 
-        var fieldset = document.createElement("fieldset")
-             .appendChild(legend)
-             .appendChild(h3)
-             .appendChild(span)
-             .appendChild(div)
-             .appendChild(divSize);
-
-        var article = document.createElement("article").appendChild(fieldset);
         foodmenu.appendChild(article);
     }
 }
